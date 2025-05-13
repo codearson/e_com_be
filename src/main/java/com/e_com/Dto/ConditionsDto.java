dd0cc9193fa6a7a798e7e6a9bd82b98a84ed85a9
Rfs12 <rfsmarsh1234@gmail.com>
2025-05-13 21:59:35 +0530

    Conditions table created

---
+++ src/main/java/com/e_com/Dto/ConditionsDto.java
@@ -0,0 +1,23 @@
+package com.e_com.Dto;
+
+import lombok.Data;
+
+/**
+ * Title: ConditionDto.java. Company: www.codearson.com Copyright: Copyright (c) 2025.
+ *
+ * @author Rifas Mohamed
+ * @date 13 May 2025
+ * @time 9:11:26 pm
+ * @version 1.0
+ **/
+
+@Data
+public class ConditionsDto {
+	
+	private Integer id;
+	
+	private String conditionType;
+	
+	private Boolean isActive;
+
+}
