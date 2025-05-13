fd6dba8f32d136f2f975e86c393513a852c59b30
Prusoth <mrprusothaman@gmail.com>
2025-05-13 20:42:57 +0530

    Feat - Create Brand Table and Save,Update,UpdateStatus,getAllPage Api

---
+++ src/main/java/com/e_com/Dto/BrandDto.java
@@ -0,0 +1,22 @@
+package com.e_com.Dto;
+
+import lombok.Data;
+
+/**
+ * Title: BrandDto.java. Company: www.codearson.com Copyright: Copyright (c) 2025.
+ *
+ * @author Purushorthaman Murugathas Rathakrishnan
+ * @date 13 May 2025
+ * @time 19:03:45
+ * @version 1.0
+ **/
+
+@Data
+public class BrandDto {
+	
+    private Integer id;
+    
+    private String brandName;
+    
+    private Boolean isActive;
+}
