0d19d77f07a8230fa0b0803f8f8797d17d253322
mshrf02 <musarrif2000@gmail.com>
2025-05-13 23:40:40 +0530

    bank table created

--- source_files/mysql/local/E_COM_DB_Structure.sql
+++ source_files/mysql/local/E_COM_DB_Structure.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: ecom_db_local
 -- ------------------------------------------------------
--- Server version	8.3.0
+-- Server version	8.0.42
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,21 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bank`
+--
+
+DROP TABLE IF EXISTS `bank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bank` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `is_active` bit(1) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `password_reset_token`
@@ -104,4 +119,4 @@ CREATE TABLE `user_role` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-13 13:27:17
+-- Dump completed on 2025-05-13 23:35:52
---
+++ src/main/java/com/e_com/Dto/BankDto.java
@@ -0,0 +1,21 @@
+package com.e_com.Dto;
+
+import lombok.Data;
+
+/**
+ * Title: BankDto.java. Company: www.codearson.com Copyright: Copyright (c) 2025.
+ *
+ * @author Asjath Musharrif Abusalif
+ * @date May 13, 2025
+ * @time 11:08:10 PM
+ * @version 1.0
+ **/
+
+@Data
+public class BankDto {
+	
+    private Integer id;
+    private String name;
+    private Boolean isActive;
+
+}
