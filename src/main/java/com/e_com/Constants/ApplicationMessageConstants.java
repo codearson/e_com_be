71ddf4327edda1b09fe35817bec54fa452cbf2d2
Rfs12 <rfsmarsh1234@gmail.com>
2025-05-15 19:47:08 +0530

    Conditions table UpdateStatus Api created

--- src/main/java/com/e_com/Constants/ApplicationMessageConstants.java
+++ src/main/java/com/e_com/Constants/ApplicationMessageConstants.java
@@ -71,6 +71,8 @@ public interface ApplicationMessageConstants {
 		String EX_SAVE_CONDITIONS_DETAILS = "ex_save_conditions_details";
 		String ERR_UPDATE_CONDITIONS_DETAILS = "err_update_conditions_details";
 		String EX_UPDATE_CONDITIONS_DETAILS = "ex_update_conditions_details";
+		String ERR_UPDATE_CONDITION_STATUS = "err_update_conditions_status";
+		String EX_UPDATE_CONDITION_STATUS = "ex_update_conditions_status";
 		String ERR_SAVE_STATUS_DETAILS = "err_save_status_details";
 		String EX_SAVE_STATUS_DETAILS = "ex_save_status_details";
 		
